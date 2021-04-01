95171d3ff8df88ebf336d9c23b52a69874ea2e57
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-04-02 00:43:28 +0530

    Added footer component

---
+++ src/data/FooterData.js
@@ -0,0 +1,52 @@
+export const ColumnOneData = [
+  {
+    category: "Academic Books",
+    path: "/academicBooks",
+  },
+  {
+    category: "Fiction Books",
+    path: "/fictionBooks",
+  },
+  {
+    category: "Non-Fiction Books",
+    path: "/non-fictionBooks",
+  },
+  {
+    category: "Children Books",
+    path: "/childrenBooks",
+  },
+];
+
+export const ColumnTwoData = [
+  {
+    category: "Self Help Books",
+    path: "/selfHelpBooks",
+  },
+  {
+    category: "Romance Books",
+    path: "/romanceBooks",
+  },
+  {
+    category: "Indian Author Books",
+    path: "/indianAuthorBooks",
+  },
+  {
+    category: "Top Selling Books",
+    path: "/topSellingBooks",
+  },
+];
+
+export const ColumnThreeData = [
+  {
+    title: "Terms And Conditions",
+    path: "/termsAndConditions",
+  },
+  {
+    title: "Privacy Policy",
+    path: "/privacyPolicy",
+  },
+  {
+    title: "Refund Policy",
+    path: "/refundPolicy",
+  },
+];
