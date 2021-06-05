e6372370d9ca6966de1670b6cb0001be38eeaa74
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-06-06 00:59:09 +0530

    Added back to top button

--- src/components/backToTop/BackToTop.jsx
+++ src/components/backToTop/BackToTop.jsx
@@ -1,40 +1,69 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import "./BackToTop.css";
+import React,{useState, useEffect} from "react";
+import styled from "styled-components";
+import { BiUpArrowAlt } from "react-icons/bi";
+import { primaryColor, darkPrimaryColor } from "../../constants/constants";
 
-const BackToTop = () => {
-  const [isVisible, setIsVisible] = useState(false);
+const BackToTop = ({showBelow}) => {
 
-  // Show button when page is scorlled upto given distance
-  const toggleVisibility = () => {
-    if (window.pageYOffset > 300) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
+  const [show,setShow] = useState(showBelow ? false : true);
+
+  const handleScroll = () => {
+    if(window.pageYOffset > showBelow){
+      if(!show) setShow(true);
+    }else{
+      if(show) setShow(false);
     }
-  };
+  }
 
-  // Set the top cordinate to 0
-  // make scrolling smooth
-  const scrollToTop = () => {
+  const handleOnClick = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth",
+      behaviour: 'smooth'
     });
-  };
+  }
 
   useEffect(() => {
-    window.addEventListener("scroll", toggleVisibility);
-  }, []);
+    if(showBelow){
+      window.addEventListener("scroll", handleScroll);
+    }
+    return () => window.removeEventListener("scroll",handleScroll);
+  })
+
   return (
-    <div id="scroll_top" onClick={scrollToTop}>
-      {isVisible && (
-        <Link to="#top">
-          <i class="fas fa-angle-up"></i>
-        </Link>
-      )}
-    </div>
+   <>
+    {
+      show && (
+         <BackToTopButton onClick={handleOnClick}>
+            <ArrowIcon />
+          </BackToTopButton>
+        )
+    }
+   </>
   );
 };
 
 export default BackToTop;
+
+const BackToTopButton = styled.div`
+  position: fixed;
+  bottom: 6rem;
+  right: 2rem;
+  z-index: 999;
+  width: 5rem;
+  height: 6rem;
+  background-color: ${primaryColor};
+  border-radius: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.5s, border 0.1s, box-shadow 0.5s;
+  cursor: pointer;
+  color: #fff;
+  font-size: 3.2rem;
+
+  :hover {
+    border: 0.3rem solid #fff;
+    box-shadow: 0 0 2rem rgba(225, 225, 225, 0.5);
+  }
+`;
+const ArrowIcon = styled(BiUpArrowAlt)``;
